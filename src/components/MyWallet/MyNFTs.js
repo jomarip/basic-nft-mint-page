b768fc7c3bbb685e458a8c834dd30658e3e5fb1b
Jomari Peterson <jomari.peterson@gmail.com>
2022-12-19 23:53:56 -0800

    Update MyNFTs.js

--- src/components/MyWallet/MyNFTs.js
+++ src/components/MyWallet/MyNFTs.js
@@ -14,7 +14,8 @@ import erc721abi from "../../contractABI/NFTABI.json";
 import axios from 'axios';
 
 const CONTRACT_ADDRESS = "0xe0290c183e9F63A6f28938051443D9Ed47710073";
-const MORALIS_KEY = process.env.PERSONMORALIS_KEY;
+
+const MORALIS_KEY = process.env.PERSONAL_MORALIS_KEY;
 
 const MyNFTs = () => {
 
